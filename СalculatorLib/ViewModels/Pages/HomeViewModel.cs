7ebfdc2c2062b6636bbabb09fffe6ecc579e1ebf
Данил Золотарёв <dan2002.dan2002@mail.ru>
2023-05-04 12:42:14 +0500

    Сделал ввод символов

--- СalculatorLib/ViewModels/Pages/HomeViewModel.cs
+++ СalculatorLib/ViewModels/Pages/HomeViewModel.cs
@@ -5,17 +5,47 @@ namespace СalculatorLib.ViewModels.Pages
 {
     public class HomeViewModel : MvxViewModel
     {
-        public IMvxCommand ResetTextCommand => new MvxCommand(ResetText);
-        private void ResetText()
+
+        #region Поля ввод/вывод
+
+        private string _inputText = "input";
+        public string InputText
         {
-            Text = "Hello MvvmCross";
+            get => _inputText;
+            set => SetProperty(ref _inputText, value);
         }
 
-        private string _text = "Hello MvvmCross";
-        public string Text
+        private string _outputText = "output";
+        public string OutputText
         {
-            get => _text;
-            set => SetProperty(ref _text, value);
+            get => _outputText;
+            set => SetProperty(ref _outputText, value);
+        }
+
+        #endregion
+
+        #region Команды
+
+        public IMvxCommand InputCharacterCommand { get; }
+
+        public IMvxCommand InputDelCommand { get; }
+
+        #endregion
+
+        public HomeViewModel()
+        {
+            #region Заполнение команд
+
+            InputCharacterCommand = new MvxCommand<char>(
+                p => InputText += p
+            );
+
+            InputDelCommand = new MvxCommand(
+                () => InputText = InputText.Substring(0, InputText.Length-1),
+                () => InputText.Length > 0
+            );
+
+            #endregion
         }
     }
 }
